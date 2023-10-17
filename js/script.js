172e2998a860761107ec7f16336512f362d02805
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-10-17 17:04:41 -0500

    added modal for city selection

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // API Key: fa09d3b5c74a7a76d7a2fb0ef78d5662
 
 
-$('#search-button').click(getAPI);
+$('#search-button').click(getLoc);
 
-function getAPI () {
+function getLoc () {
     var apiURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + $('#search-field').val() +'&limit=5&appid=fa09d3b5c74a7a76d7a2fb0ef78d5662'
 
     console.log(apiURL);
@@ -14,9 +14,31 @@ function getAPI () {
         })
         .then(function (data) {
             console.log(data);
+
+            for (var i=0; i<data.length; i++) {
+            var cityOpt = $('<button></button>').text(data[i].name + ', ' + data[i].state + ', ' + data[i].country).data('lat', data[i].lat).data('lon', data[i].lon).attr('id', 'city' + i);
+
+            $('#modal').append(cityOpt);
+
+            console.log(cityOpt);
+            };
+
+            $('#modal').removeClass('d-none');
         })
         .catch(err => {
             console.error(err);
         });
 
 }
+
+
+
+function selectCity () {
+
+}
+
+function closeModal () {
+    $('#modal').addClass('d-none');
+}
+
+$('#close').click(closeModal);
